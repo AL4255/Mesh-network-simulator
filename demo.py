d9a139f17c9020afde9997d10e2384cd38039647
AL4255 <92372109+AL4255@users.noreply.github.com>
2026-01-22 18:12:20 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -8,3 +8,15 @@ import time
 import logging
 from mesh_node import MeshNode
 from mesh_protocol import MessageType
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+
+def demo_basic_network():
+    """Demo: Basic 3-node network with authentication and messaging"""
+    print("\n" + "="*60)
+    print("DEMO: Basic Mesh Network with Authentication")
+    print("="*60 + "\n")
